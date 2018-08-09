6d10e89777856700479d923d6dc1ce0da570b2f3
Gabriel Gebril <Gebril.Gabriel@gmail.com>
2018-08-09 15:08:07 -0400

    Random Generation of items using faker. Just to test styles.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var app = express();
+var faker = require('faker');
+
 app.set("view engine", "ejs");
 app.use(express.static('./public'));
 app.use(express.static('./controllers'));
 
 app.get("/", function(req,res){
-    res.render("index");
+    var randomProduct = [];
+    for(var i = 0; i<80; i++){
+        randomProduct.push(faker.commerce.productName())
+    }
+    var randomTotal = Math.random() * 1000
+    var randomAv = Math.random * randomTotal;
+    res.render("index", {randomProduct : randomProduct, randomTotal: randomTotal,
+     randomAv: randomAv});
 });
 
 app.listen(3000,console.log("Server on port 3000."));
