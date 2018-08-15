6ecef70d8313d9b3fb5088d4c160876c5f458771
Gabriel Gebril <Gebril.Gabriel@gmail.com>
2018-08-14 21:41:46 -0400

    Navbar fixes

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ app.set("view engine", "ejs");
 app.use(index);
 
 
-var newI = {
-    name: "test item j",
-    instock: 5,
-    total: 500,
-    description: "awesome item",
-    signed_out_by: "Gabriel"
-}
+// var newI = {
+//     name: "test item j",
+//     instock: 5,
+//     total: 500,
+//     description: "awesome item",
+//     signed_out_by: "Gabriel"
+// }
 
-item.create(newI, function(result){
-    console.log(result);
-});
+// item.create(newI, function(result){
+//     console.log(result);
+// });
 
 // for (let index = 0; index < 100; index++) {
 //     item.create({
