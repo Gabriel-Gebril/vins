54eeec23348b1097b3e087013d5582150b15d39b
Gabriel Gebril <Gebril.Gabriel@gmail.com>
2018-08-16 17:54:30 -0400

    Add Item form changed to work on mobile.

--- public/formAdd.js
+++ public/formAdd.js
@@ -17,6 +17,12 @@ $(document).ready(function(){
             return true;
         }
     });
+
+    $("#cbox").on('input', function() {
+        var scroll_height = $("#message-box").get(0).scrollHeight;
+    
+        $("#message-box").css('height', scroll_height + 'px');
+    });
 });
 
 
